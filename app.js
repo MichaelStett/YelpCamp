0fbecb9f5b6ea149ee5a3ea25174473fa0156465
Michal Tymejczyk <michal.tymejczyk@gmail.com>
2019-06-25 19:46:48 +0200

    Campgrounds

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const parser = require('body-parser');
 const app = express();
 
 const PORT = 3000;
 const HOST = '127.0.0.1';
 
+var campgrounds = [
+    { name: "Salomon Creek", image: "https://live.staticflickr.com/5542/11936566655_2535c3606a_k.jpg" },
+    { name: "Granite Hill", image: "https://live.staticflickr.com/7169/6401974259_459a877dd4_b.jpg" },
+    { name: "Mountian Goat's Rest", image: "https://live.staticflickr.com/3691/12005576473_5c938e610a_b.jpg" }
+];
+
+app.use(parser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
@@ -11,15 +19,25 @@ app.get('/', (req, res) => {
 });
 
 app.get('/campgrounds', (req, res) => {
-    const campgrounds = [
-        { name: "Salomon Creek", image: "https://live.staticflickr.com/5542/11936566655_2535c3606a_k.jpg" },
-        { name: "Granite Hill", image: "https://live.staticflickr.com/7169/6401974259_459a877dd4_b.jpg" },
-        { name: "Mountian Goat's Rest", image: "https://live.staticflickr.com/3691/12005576473_5c938e610a_b.jpg" }
-    ];
-
     res.render('campgrounds', { 
         campgrounds: campgrounds 
     });
+});
+
+app.post('/campgrounds', (req, res) => {
+    const name = req.body.name;
+    const image = req.body.image;
+
+    campgrounds.push({
+        name: name, 
+        image: image
+    });
+    
+    res.redirect('campgrounds');
+});
+
+app.get('/campgrounds/new', (req, res) => {
+    res.render('new');
 });
 
 app.get('*', (req, res) => {
