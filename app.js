19e26e68de70f145da63989ae8dda16da9678751
MichaelStett <michal.tymejczyk@gmail.com>
2019-07-19 17:06:32 +0200

    Added MongoDB

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const app = express();
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
+var db = require('mongoose');
+db.connect("mongodb://mongo:27017");
+
 var campgrounds = [
     { name: "Salomon Creek", image: "https://live.staticflickr.com/5542/11936566655_2535c3606a_k.jpg" },
     { name: "Granite Hill", image: "https://live.staticflickr.com/7169/6401974259_459a877dd4_b.jpg" },
@@ -15,8 +18,6 @@ var campgrounds = [
     { name: "Salomon Creek", image: "https://live.staticflickr.com/5542/11936566655_2535c3606a_k.jpg" },
     { name: "Granite Hill", image: "https://live.staticflickr.com/7169/6401974259_459a877dd4_b.jpg" },
     { name: "Mountian Goat's Rest", image: "https://live.staticflickr.com/3691/12005576473_5c938e610a_b.jpg" }
-
-
 ];
 
 app.use(parser.urlencoded({extended: true}));
