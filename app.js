b3f62221e3a5a704952405346a2fc1b8ac0871b2
Michal Tymejczyk <michal.tymejczyk@gmail.com>
2019-07-25 23:23:05 +0200

    Show and Add campground images MongoDB

--- app.js
+++ app.js
@@ -1,47 +1,58 @@
-const express = require('express');
-const parser = require('body-parser');
-const app = express();
+const express = require('express'),
+    parser = require('body-parser'),
+    mongoose = require('mongoose'),
+    app = express();
 
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
-var db = require('mongoose');
-db.connect("mongodb://mongo:27017");
-
-var campgrounds = [
-    { name: "Salomon Creek", image: "https://live.staticflickr.com/5542/11936566655_2535c3606a_k.jpg" },
-    { name: "Granite Hill", image: "https://live.staticflickr.com/7169/6401974259_459a877dd4_b.jpg" },
-    { name: "Mountian Goat's Rest", image: "https://live.staticflickr.com/3691/12005576473_5c938e610a_b.jpg" },
-    { name: "Salomon Creek", image: "https://live.staticflickr.com/5542/11936566655_2535c3606a_k.jpg" },
-    { name: "Granite Hill", image: "https://live.staticflickr.com/7169/6401974259_459a877dd4_b.jpg" },
-    { name: "Mountian Goat's Rest", image: "https://live.staticflickr.com/3691/12005576473_5c938e610a_b.jpg" },
-    { name: "Salomon Creek", image: "https://live.staticflickr.com/5542/11936566655_2535c3606a_k.jpg" },
-    { name: "Granite Hill", image: "https://live.staticflickr.com/7169/6401974259_459a877dd4_b.jpg" },
-    { name: "Mountian Goat's Rest", image: "https://live.staticflickr.com/3691/12005576473_5c938e610a_b.jpg" }
-];
-
-app.use(parser.urlencoded({extended: true}));
+app.use(parser.urlencoded({
+    extended: true
+}));
 app.set('view engine', 'ejs');
+
+mongoose.connect("mongodb://mongo:27017/yelp_camp");
+
+const campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String
+});
+
+const Campground = mongoose.model("Campground", campgroundSchema);
+
+
 
 app.get('/', (req, res) => {
     res.render('landing');
 });
 
 app.get('/campgrounds', (req, res) => {
-    res.render('campgrounds', { 
-        campgrounds: campgrounds 
-    });
+    Campground.find({}, (err, all) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("campgrounds", {
+                campgrounds: all
+            })
+        }
+    })
 });
 
 app.post('/campgrounds', (req, res) => {
     const name = req.body.name;
     const image = req.body.image;
 
-    campgrounds.push({
-        name: name, 
+    Campground.create({
+        name: name,
         image: image
+    }, (err, obj) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(obj._doc)
+        }
     });
-    
+
     res.redirect('campgrounds');
 });
 
